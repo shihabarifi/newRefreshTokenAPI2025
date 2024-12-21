df30f54c593d4a9e04e26315e3ee87589dcc3e5d
shihabarifi <shihabriotkd@gmail.com>
2024-12-22 01:38:04 +0300

    Add project files.

---
+++ newRefreshTokenAPI/Models/RefreshToken.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace newRefreshTokenAPI.Models
+{
+    public class RefreshToken
+    {
+        public string? Token {get;set;}
+        public string? JWTToken { get;set; }
+        public DateTime Created {get;set;}
+        public DateTime Expires {get;set;}
+    }
+}
